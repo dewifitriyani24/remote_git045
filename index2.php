c9f9c35273fd9134f9484f3d9acd66284116e0af
dewifitriyani24 <wid84731@gmail.com>
2024-06-13 14:49:43 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index 2 GIT</title>
+</head>
+<body>
+    <H1>Latihan Remote GIThub-git</H1>
+    <h1>kelas 22 mi b</h1>
+    
+</body>
+</html>
